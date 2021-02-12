be4734a3f5859aaa1817bed212ab4e328234e152
vincentsimonmarufu <vincentsimonmarufu@gmail.com>
2021-02-12 15:56:54 +0200

    updated follow page

--- resources/views/app-home/follow.blade.php
+++ resources/views/app-home/follow.blade.php
@@ -45,7 +45,7 @@
                                         <th>date attended</th>
                                         <th>status</th>
                                         <th>attended by</th>
-                                        <th class="no-content"></th>
+                                        
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -82,7 +82,7 @@
                                                 {{ $item->resolved_by }}
                                             @endif
                                         </td>
-                                        <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle table-cancel"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg></td>
+                                        
                                     </tr>
                                     @endforeach
                                 </tbody>
